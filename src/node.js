e61e352716fd150f690a0e077bcd86f51973acf7
= <chechenyshka@mail.ru>
2017-10-03 14:17:29 +0300

    create some functions

--- src/node.js
+++ src/node.js
@@ -8,19 +8,56 @@ class Node {
     }
 
     appendChild(node) {
+        if (this.left && !this.right) {
+            this.right = node;
+            this.right.parent = this;
+        }
+        if (!this.left) {
+            this.left = node;
+            this.left.parent = this;
+        }
 
     }
 
     removeChild(node) {
-
+        if (this.left == node) {
+            this.left.parent = null
+            this.left = null;
+        } else {
+            if (this.right == node) {
+                this.right.parent = null;
+                this.right = null;
+            } else {
+                throw new Error("there isnt a child");
+            }
+        }
     }
 
     remove() {
-
+        if (this.parent) {
+            this.parent.removeChild(this);
+        }
     }
 
     swapWithParent() {
+        let temp;
+        if (this.parent) {
+            //left
+            temp = this.left;
+            this.left = this.parent;
+            this.parent.left = temp;
+            //right
+            temp = this.right;
+            this.right = this.parent.right;
+            this.parent.right = temp;
+            /*  //parent
+              temp = this.parent.parent;
+              this.parent.parent = this.parent;
+              this.parent = temp;*/
 
+
+
+        }
     }
 }
 
