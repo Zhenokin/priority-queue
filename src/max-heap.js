85c196b0cabbd767e965f7106c0053cb67e05704
= <chechenyshka@mail.ru>
2017-10-04 22:36:01 +0300

    realized 'insertNode'

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,49 +1,70 @@
 const Node = require('./node');
 
 class MaxHeap {
-	constructor() {
-		
-	}
+    constructor() {
+        this.root = null;
+        this.parentNodes = [];
+        this._size = 0;
+    }
 
-	push(data, priority) {
-		
-	}
+    push(data, priority) {
+        const node = new Node(data, priority);
+        this.insertNode(node);
+        this.shiftNodeUp(node);
+    }
 
-	pop() {
-		
-	}
+    pop() {
 
-	detachRoot() {
-		
-	}
+    }
 
-	restoreRootFromLastInsertedNode(detached) {
-		
-	}
+    detachRoot() {
 
-	size() {
-		
-	}
+    }
 
-	isEmpty() {
-		
-	}
+    restoreRootFromLastInsertedNode(detached) {
 
-	clear() {
-		
-	}
+    }
 
-	insertNode(node) {
-		
-	}
+    size() {
+        return this._size;
 
-	shiftNodeUp(node) {
-		
-	}
+    }
 
-	shiftNodeDown(node) {
-		
-	}
+    isEmpty() {
+        if (!this._size) {
+            return true;
+        }
+        return false;
+
+    }
+
+    clear() {
+        this.root = null;
+        this.parentNodes = [];
+
+    }
+
+    insertNode(node) {
+        if (!this.root) {
+            this.root = node;
+            this.parentNodes.push(this.root);
+        } else {
+            this.parentNodes[0].appendChild(node);
+            this.parentNodes.push(node);
+            if (this.parentNodes[0].right) {
+                this.parentNodes.shift();
+            }
+        }
+
+    }
+
+    shiftNodeUp(node) {
+
+    }
+
+    shiftNodeDown(node) {
+
+    }
 }
 
 module.exports = MaxHeap;
